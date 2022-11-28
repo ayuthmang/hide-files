232d0be2abab80f7c9f06844b7e0a8fa8e2c9616
Ayuth Mangmesap <ayuth.mang@gmail.com>
2022-11-28 21:42:29 +0700

    :tada: add working app! :tada:

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+
+import { program } from 'commander'
+import chalk from 'chalk'
+import { spawn } from 'child_process'
+import { homedir } from 'os'
+import { join, sep } from 'path'
+
+program
+  .command('hide')
+  .description('Hide all files and folders in desktop')
+  .action(() => {
+    console.log(
+      `Your files and folders have been ${chalk.yellow('hidden! 🤫')}`
+    )
+    const desktopPath = join(homedir(), 'Desktop', sep)
+    spawn('chflags', ['hidden', `${desktopPath.replaceAll(' ', `" "`)}*`], {
+      shell: true,
+    })
+  })
+
+program
+  .command('unhide')
+  .description('Unhide all files and folders in desktop')
+  .action(() => {
+    console.log(`Your files and folders have been ${chalk.yellow('shown! 🫣')}`)
+    const desktopPath = join(homedir(), 'Desktop', sep)
+    spawn('chflags', ['nohidden', `${desktopPath.replaceAll(' ', `" "`)}*`], {
+      shell: true,
+    })
+  })
+
+program.parse()
